085abee1fd861d5b05c64e35b52dfc919ba58856
wxc <xuechaowu@dartou.com>
2022-08-30 16:40:59 +0800

    feat: 项目初始化

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // 末尾不需要逗号 'es5' none
+    trailingComma: 'es5',
+    // 大括号内的首尾需要空格
+    bracketSpacing: true,
+    // 行尾需要有分号
+    semi: true,
+    // 使用单引号
+    singleQuote: true,
+    // 一行最多 100 字符
+    printWidth: 180,
+    // 不使用缩进符，而使用空格
+    useTabs: false,
+    // 使用 4 个空格缩进
+    tabWidth: 4,
+    // 换行符使用 lf 结尾是 \n \r \n\r auto
+    endOfLine: 'lf',
+}
